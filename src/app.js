480f6465e8c37405e5568cb68693172e7fe0a15c
FabioCeleste <36655978+FabioCeleste@users.noreply.github.com>
2020-04-02 02:06:47 -0300

    start

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import dotenv from 'dotenv';
+import { resolve } from 'path';
+
+dotenv.config();
+
+import './database';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import tokenRoutes from './routes/tokenRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+  'http://34.95.211.180',
+  'http://localhost:3000',
+  'https://www.twitch.tv',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed'));
+    }
+  },
+};
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+    this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
+    this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
+  }
+}
+
+export default new App().app;
